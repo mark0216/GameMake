9664089a304e5b0fad98706b18796284fc165844
mark0216 <mark02160216@gmail.com>
2023-01-05 18:04:34 +0800

    0

--- Assets/03.Scripts/Spell/BaseSpellTrigger.cs
+++ Assets/03.Scripts/Spell/BaseSpellTrigger.cs
@@ -9,9 +9,11 @@ public abstract class BaseSpellTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {
-            print(gameObject.name + " hit");
             HitPlayer();
-        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            HitPlayer();
     }
 }
