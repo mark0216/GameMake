667752a51ea98e1fbb6a086ab95c0ee91c321e67
Luncere <99242361+Luncere@users.noreply.github.com>
2023-01-09 01:13:00 +0800

    manaBottle

---
+++ Assets/03.Scripts/Spell/ManaBottle_Control.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBottle_Control : MonoBehaviour
+{
+    [SerializeField] private Text ClickText;
+    [SerializeField] private Text ManaText;
+    [SerializeField] private float ClickTime = 10;
+    [SerializeField] private float ManaValue = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refresh();
+    }
+    void OnMouseDown()
+    {
+        ClickTime--;
+        refresh();
+        if (ClickTime <= 0)
+        {
+            doGainMana(ManaValue);
+        }
+    }
+    private void refresh()
+    {
+        ClickText.text = ClickTime.ToString();
+        ManaText.text = ManaValue.ToString();
+    }
+    private void doGainMana(float ManaValue)
+    {
+        Debug.Log("Gain " + ManaValue.ToString()+ " Mana");
+        DestoryEvent();
+    }
+    public void DestoryEvent()
+    {
+        Destroy(this.gameObject);
+    }
+}
