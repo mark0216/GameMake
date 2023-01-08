222e6a81b95181f92056c03e138656ba56561192
mark0216 <mark02160216@gmail.com>
2023-01-09 00:45:03 +0800

    p

--- Assets/03.Scripts/Spell/rickroller_Control.cs
+++ Assets/03.Scripts/Spell/rickroller_Control.cs
@@ -5,21 +5,13 @@ using UnityEngine;
 public class rickroller_Control : BaseSpellTrigger
 {
     [SerializeField] private GameObject videoPlayer;
-    [SerializeField] private float dropSpeed;
-
-    private void Start()
-    {
-        transform.position += new Vector3(0, 25, 0);
-    }
-    private void Update()
-    {
-        transform.position -= new Vector3(0, dropSpeed * Time.deltaTime, 0);
-        if (transform.position.y < GameObject.Find("bottom").transform.position.y)
-            Destroy(this.transform.parent.gameObject);
-    }
     protected override void HitPlayer()
     {
         Instantiate(videoPlayer);
         GetComponent<BoxCollider2D>().enabled = false;
+    }
+    public void DestoryEvent()
+    {
+        Destroy(this.gameObject);
     }
 }
